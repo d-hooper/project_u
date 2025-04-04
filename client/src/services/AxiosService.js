7a2ac34d3e9cfe01e795e36d70b36e502a481593
Roman Wernsing <wernsingroman@gmail.com>
2025-04-04 16:56:20 -0600

    serving counter working well

--- client/src/services/AxiosService.js
+++ client/src/services/AxiosService.js
@@ -10,7 +10,7 @@ export const api = Axios.create({
 export const spoonacularApi = Axios.create({
   baseURL: 'https://api.spoonacular.com',
   params: {
-    apiKey: '98643d1c6cde40ed94778e426ad571aa'
+    apiKey: 'fbc0950b95f544d58eddaee823797745'
   },
   timeout: 3000
 })
