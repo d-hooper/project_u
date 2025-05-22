92dac1e4afdbbde54fe003cd4f716af5733c46e2
d-hooper <dallinhoop3@gmail.com>
2025-05-22 17:12:26 -0600

    updated env

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'dev-1eliqcykgosr1ydc.us.auth0.com'
-export const clientId = 'JaEv0p3AivYE08AHm6OWUkQgLYkOK8hF'
-export const audience = 'https://auth.RomansFirstApp.com'
+export const domain = 'dev-5x00quh2000bn67r.us.auth0.com'
+export const clientId = 'rIgFRlgvMf2EjU8FMoLrmIe6jiycAVT5'
+export const audience = 'https://auth.speedwagonfoundation.com'
